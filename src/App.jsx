2ea03669a9e543d81a47c80734e49bd26c9712db
cayoshi <nateeronron@gmail.com>
2024-10-18 17:58:55 +0700

    dd

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,389 @@
-import { useState } from 'react'
+//npm install @mui/material @emotion/react @emotion/styled
 
+import React, { useState } from "react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import Checkbox from "@mui/material/Checkbox";
 
-function App() {
-  const [count, setCount] = useState(0)
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
-  return (
-    <>
-     OK Run UI React
-    </>
-  )
+import TextField from "@mui/material/TextField";
+
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
+import { styled } from "@mui/material/styles";
+
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+
+import PropTypes from "prop-types";
+import Grid from "@mui/material/Grid";
+import Skeleton from "@mui/material/Skeleton";
+
+const data = [
+    {
+        src: "https://i.ytimg.com/vi/pLqipJNItIo/hqdefault.jpg?sqp=-oaymwEYCNIBEHZIVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLBkklsyaw9FxDmMKapyBYCn9tbPNQ",
+        title: "Don Diablo @ Tomorrowland Main Stage 2019 | Official…",
+        channel: "Don Diablo",
+        views: "396k views",
+        createdAt: "a week ago",
+    },
+    {
+        src: "https://i.ytimg.com/vi/_Uu12zY01ts/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLCpX6Jan2rxrCAZxJYDXppTP4MoQA",
+        title: "Queen - Greatest Hits",
+        channel: "Queen Official",
+        views: "40M views",
+        createdAt: "3 years ago",
+    },
+    {
+        src: "https://i.ytimg.com/vi/kkLk2XWMBf8/hqdefault.jpg?sqp=-oaymwEYCNIBEHZIVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLB4GZTFu1Ju2EPPPXnhMZtFVvYBaw",
+        title: "Calvin Harris, Sam Smith - Promises (Official Video)",
+        channel: "Calvin Harris",
+        views: "130M views",
+        createdAt: "10 months ago",
+    },
+];
+
+function createData(name, calories, fat, carbs, protein) {
+    return { name, calories, fat, carbs, protein };
 }
 
-export default App
+const rows = [
+    createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+    createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+    createData("Eclair", 262, 16.0, 24, 6.0),
+    createData("Cupcake", 305, 3.7, 67, 4.3),
+    createData("Gingerbread", 356, 16.0, 49, 3.9),
+];
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+        backgroundColor: theme.palette.common.black,
+        color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+        fontSize: 14,
+    },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    "&:nth-of-type(odd)": {
+        backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    "&:last-child td, &:last-child th": {
+        border: 0,
+    },
+}));
+
+const bull = (
+    <Box component="span" sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}>
+        •
+    </Box>
+);
+
+
+function Media(props) {
+  const { loading = false } = props;
+
+  return (
+    <Grid container wrap="nowrap">
+      {(loading ? Array.from(new Array(3)) : data).map((item, index) => (
+        <Box key={index} sx={{ width: 210, marginRight: 0.5, my: 5 }}>
+          {item ? (
+            <img
+              style={{ width: 210, height: 118 }}
+              alt={item.title}
+              src={item.src}
+            />
+          ) : (
+            <Skeleton variant="rectangular" width={210} height={118} />
+          )}
+          {item ? (
+            <Box sx={{ pr: 2 }}>
+              <Typography gutterBottom variant="body2">
+                {item.title}
+              </Typography>
+              <Typography
+                variant="caption"
+                sx={{ display: 'block', color: 'text.secondary' }}
+              >
+                {item.channel}
+              </Typography>
+              <Typography variant="caption" sx={{ color: 'text.secondary' }}>
+                {`${item.views} • ${item.createdAt}`}
+              </Typography>
+            </Box>
+          ) : (
+            <Box sx={{ pt: 0.5 }}>
+              <Skeleton />
+              <Skeleton width="60%" />
+            </Box>
+          )}
+        </Box>
+      ))}
+    </Grid>
+  );
+}
+
+
+export default function App() {
+    const label = { inputProps: { "aria-label": "Checkbox demo" } };
+    const [age, setAge] = React.useState("");
+
+    const handleChange = (event) => {
+        setAge(event.target.value);
+    };
+    const [mode, setMode] = useState("dark"); // light  dark
+    const darkTheme = createTheme({
+        palette: {
+            mode: mode,
+            ...(mode === "dark"
+                ? {
+                      // palette values for dark mode
+                      primary: {
+                          dark: "#2C5C8BFF",
+                          main: "#0D70E2FF",
+                          light: "#922020FF",
+                      },
+                      secondary: {
+                          main: "#65BB15FF",
+                      },
+                      neutral: {
+                          dark: "#2B2B2BFF",
+                          main: "#910808FF",
+                          light: "#EBEBEBFF",
+                      },
+                      background: {
+                          default: "#161616FF",
+                      },
+                      text: {
+                          primary: "#E4E4E4FF",
+                          secondary: "#B6B6B6FF",
+                      },
+                  }
+                : {
+                      // palette values for light mode
+                      primary: {
+                          dark: "#6B215BFF",
+                          main: "#91287AFF",
+                          light: "#E665CAFF",
+                      },
+                      secondary: {
+                          dark: "#4E1E64CE",
+                          main: "#7E3C9CCE",
+                          light: "#B650E6CE",
+                      },
+                      neutral: {
+                          dark: "#F0F0F0FF",
+                          main: "#F0F0F0FF",
+                          light: "#F0F0F0FF",
+                      },
+                      background: {
+                          default: "#fcfcfc",
+                      },
+                      text: {
+                          primary: "#2E2D2DFF",
+                          secondary: "#4E4E4EFF",
+                      },
+                  }),
+        },
+        typography: {
+            fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+            fontSize: 12,
+            h1: {
+                fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+                fontSize: 40,
+            },
+            h2: {
+                fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+                fontSize: 32,
+            },
+            h3: {
+                fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+                fontSize: 24,
+            },
+            h4: {
+                fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+                fontSize: 20,
+            },
+            h5: {
+                fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+                fontSize: 16,
+            },
+            h6: {
+                fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+                fontSize: 14,
+            },
+        },
+    });
+
+
+    return (
+        <div className="Container">
+            <label>TEST</label>
+            ชีวิตไม่ต้องเด่น ขอแค่เป็นเทพในเงา
+            <ThemeProvider theme={darkTheme}>
+                <CssBaseline />
+                <Stack spacing={2} direction="row">
+                    <Button variant="text">Text</Button>
+                    <Button variant="contained">Contained</Button>
+                    <Button variant="outlined">Outlined</Button>
+
+                    <div>
+                        <Checkbox {...label} defaultChecked />
+                        <Checkbox {...label} />
+                        <Checkbox {...label} disabled />
+                        <Checkbox {...label} disabled checked />
+                    </div>
+                    <Box sx={{ minWidth: 120 }}>
+                        <FormControl fullWidth>
+                            <InputLabel id="demo-simple-select-label">Age</InputLabel>
+                            <Select
+                                labelId="demo-simple-select-label"
+                                id="demo-simple-select"
+                                value={age}
+                                label="Age"
+                                onChange={handleChange}
+                            >
+                                <MenuItem value={10}>Ten</MenuItem>
+                                <MenuItem value={20}>Twenty</MenuItem>
+                                <MenuItem value={30}>Thirty</MenuItem>
+                            </Select>
+                        </FormControl>
+                    </Box>
+                </Stack>
+                <div>
+                    <h1>h1 aaaaaaaaaaaaaaaaa</h1>
+                    <h2>h2 aaaaaaaaaaaaaaaaa</h2>
+                    <h3>h3 aaaaaaaaaaaaaaaaa</h3>
+                    <h4>h4 aaaaaaaaaaaaaaaaa</h4>
+                    <h5>h5 aaaaaaaaaaaaaaaaa</h5>
+                    <h6>h6 aaaaaaaaaaaaaaaaa</h6>
+                </div>
+                <Box component="form" sx={{ "& > :not(style)": { m: 1, width: "25ch" } }} noValidate autoComplete="off">
+                    <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+                    <TextField id="filled-basic" label="Filled" variant="filled" />
+                    <TextField id="standard-basic" label="Standard" variant="standard" />
+                </Box>
+
+                <TableContainer component={Paper}>
+                    <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Dessert (100g serving)</TableCell>
+                                <TableCell align="right">Calories</TableCell>
+                                <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                                <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                                <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {rows.map((row) => (
+                                <TableRow key={row.name} sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
+                                    <TableCell component="th" scope="row">
+                                        {row.name}
+                                    </TableCell>
+                                    <TableCell align="right">{row.calories}</TableCell>
+                                    <TableCell align="right">{row.fat}</TableCell>
+                                    <TableCell align="right">{row.carbs}</TableCell>
+                                    <TableCell align="right">{row.protein}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+                <br></br>
+                <TableContainer component={Paper}>
+                    <Table sx={{ minWidth: 700 }} aria-label="customized table">
+                        <TableHead>
+                            <TableRow>
+                                <StyledTableCell>Dessert (100g serving)</StyledTableCell>
+                                <StyledTableCell align="right">Calories</StyledTableCell>
+                                <StyledTableCell align="right">Fat&nbsp;(g)</StyledTableCell>
+                                <StyledTableCell align="right">Carbs&nbsp;(g)</StyledTableCell>
+                                <StyledTableCell align="right">Protein&nbsp;(g)</StyledTableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {rows.map((row) => (
+                                <StyledTableRow key={row.name}>
+                                    <StyledTableCell component="th" scope="row">
+                                        {row.name}
+                                    </StyledTableCell>
+                                    <StyledTableCell align="right">{row.calories}</StyledTableCell>
+                                    <StyledTableCell align="right">{row.fat}</StyledTableCell>
+                                    <StyledTableCell align="right">{row.carbs}</StyledTableCell>
+                                    <StyledTableCell align="right">{row.protein}</StyledTableCell>
+                                </StyledTableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+                <br></br>
+                <br></br>
+                <Card sx={{ minWidth: 275 }}>
+                    <CardContent>
+                        <Typography gutterBottom sx={{ color: "text.secondary", fontSize: 14 }}>
+                            Word of the Day
+                        </Typography>
+                        <Typography variant="h5" component="div">
+                            be{bull}nev{bull}o{bull}lent
+                        </Typography>
+                        <Typography sx={{ color: "text.secondary", mb: 1.5 }}>adjective</Typography>
+                        <Typography variant="body2">
+                            well meaning and kindly.
+                            <br />
+                            {'"a benevolent smile"'}
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button size="small">Learn More</Button>
+                    </CardActions>
+                </Card>
+                <br></br>
+                <Box
+                    sx={{
+                        display: "flex",
+                        flexWrap: "wrap",
+                        "& > :not(style)": {
+                            m: 1,
+                            width: 128,
+                            height: 128,
+                        },
+                    }}
+                >
+                    <Paper elevation={0} />
+                    <Paper />
+                    <Paper elevation={3} />
+                </Box>
+                <br></br>
+
+                <Box sx={{ overflow: 'hidden' }}>
+      <Media loading />
+      <Media />
+    </Box>
+                <br></br>
+
+
+            </ThemeProvider>
+        </div>
+    );
+}
+Media.propTypes = {
+  loading: PropTypes.bool,
+};
